3ea92cb5a768e3de80cd23321a4540205f15b558
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-08-20 14:14:01 +0530

    project completed

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const userRoutes = require("./users");
+const postRoutes = require("./posts");
+const courseRoutes = require("./courses");
+const authRoutes = require("./auth");
+
+// routes
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/courses", courseRoutes);
+router.use("/auth", authRoutes);
+
+module.exports = router;
